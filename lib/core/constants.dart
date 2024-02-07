dc4337daf68b328acb5e69f2baebe8f29ab4f756
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2024-02-07 12:37:40 +0200

    Package Quantity Fix

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,4 +1,4 @@
 class Constants {
-  // static const String baseUrl = "https://smartstudent.live/api/";
-  static const String baseUrl = "https://1207-105-37-252-145.ngrok-free.app/api/";
+  static const String baseUrl = "https://smartstudent.live/api/";
+  // static const String baseUrl = "https://1207-105-37-252-145.ngrok-free.app/api/";
 }
