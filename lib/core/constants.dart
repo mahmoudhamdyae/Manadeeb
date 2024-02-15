e0fd67fd75dac3a383f674000fcba51811b97f73
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2024-02-15 11:31:28 +0200

    App Name

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,4 +1,4 @@
 class Constants {
-  // static const String baseUrl = "https://smartstudent.live/api/";
-  static const String baseUrl = "https://8ec9-105-33-117-146.ngrok-free.app/api/";
+  static const String baseUrl = "https://smartstudent.live/api/";
+  // static const String baseUrl = "https://8ec9-105-33-117-146.ngrok-free.app/api/";
 }
